b65d0e0381205780d71f3650f81ed57e019d9c98
DESKTOP-1FM26GK\NEAR <mail_km@hotmail.com>
2017-12-21 18:07:51 +0700

    เอารูป hero มาใส่

--- src/app/hero-list/hero-list.component.ts
+++ src/app/hero-list/hero-list.component.ts
@@ -10,12 +10,35 @@ import {HeroService} from '../hero.service';
 export class HeroListComponent implements OnInit {
 
   heroes:any = [];
+  img:any = [];
   constructor(private heroService: HeroService) { }
 
   ngOnInit() {
   	this.heroService.findAll().subscribe(respone =>{
-     //  console.log( respone.data);
-  		this.heroes = respone;
+      // console.log( respone.data );
+      let char_name = [];
+      var check = 0;
+      let  char_array =[];
+       respone.forEach(function (data,index) {
+          char_array = data.name.split("_");
+          if(char_array[3] != ""){
+              char_name[index] = char_array[3];
+          }
+          if(char_array[4] != null){
+              char_name[index] += "_"+char_array[4];
+          }
+
+            respone.push(      
+            {  name: char_name[index] }
+            //ติดตรงนี้นะ
+        );
+          
+      }, this);
+       //this.img = char_name;
+       //respone.name_heroes = char_name;
+     
+       console.log(respone);
+  		 this.heroes = respone;
   	});
   }
 
